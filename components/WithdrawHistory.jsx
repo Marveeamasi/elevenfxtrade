cbe160249ca3fc3c9720b53d13d3f3c9bed67570
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-09-25 07:01:47 +0100

    Update WithdrawHistory.jsx

--- components/WithdrawHistory.jsx
+++ components/WithdrawHistory.jsx
@@ -1,64 +1,128 @@
 'use client'
-import React, { useContext, useEffect, useState } from 'react'
-import Withdrawal from './Withdrawal'
-import usePagination from '@/hooks/usePagination'
+import Sidebar from '@/components/Sidebar'
+import Topbar from '@/components/Topbar'
 import { AuthContext } from '@/context/AuthContext';
-import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '@/firebase';
-import Empty from './Empty';
+import emailjs from '@emailjs/browser';
+import { arrayUnion, doc, Timestamp, updateDoc } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react'
+import CountUp from 'react-countup';
 
-export default function WithdrawHistory() {
-    const [withdrawal, setWithdrawal] = useState([]);
-    const {currentUser} = useContext(AuthContext) 
-    useEffect(() => {
-      const runFetch=async()=>{
-        try{
-      onSnapshot(
-        doc(db, 'userWithdrawals', currentUser?.uid),
-        (docSnapshot) => {
-          if (docSnapshot?.exists) {
-            const requests = docSnapshot?.data()?.withdrawals || [];
-            setWithdrawal(requests);
-          } else {
-            console.error("User withdrawals document not found");
-          }
-        },
-        (error) => {
-          console.error("Error fetching withdrawals data:", error);
-        }
-      );
-    }catch(err){
-      console.log(err)
+export default function page({params}) {
+  const {currentUser} = useContext(AuthContext) 
+  if(!currentUser){ window.location.href = '/login' }
+  const currentAmount = params.id.split('__')[0];
+  const overdue = parseFloat(params.id.split('__')[1]);
+  const underdue = parseFloat(params.id.split('__')[2]); 
+  const currentId = params.id.split('__')[3];
+  const userId = params.id.split('__')[4];
+  const plan= params.id.split('__')[5];
+  const initial = params.id.split('__')[6];
+  const [selectedOption, setSelectedOption] = useState('');
+  const [address, setAddress] = useState("");
+  const [loading , setLoading] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [enablebtn, setEnablebtn] = useState(false)
+  const profit = parseFloat(currentAmount) - parseFloat(initial);
+
+  useEffect(()=>{
+    if(!address || !selectedOption || !amount || amount>profit){
+      setEnablebtn(false);
+      if(amount>profit){
+         alert("You can't withdraw more than your profit: $"+profit)
+      }
+    }else{
+      setEnablebtn(true);
     }
+   },[address, amount, profit, selectedOption]);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
   };
-  
-      return () => runFetch();
-    }, [currentUser?.uid]);
 
-
-    const { currentPage, slicedData, totalPages, handlePageChange } = usePagination(withdrawal);
+  const handleWithdraw = async() => {
+    try {
+      setLoading(true);
+      const templateParams = {
+        from_name: '4Elevenfxtrade',
+        reply_to: currentUser.email,
+        to_email:'info@4xeleventrade.store',
+        page_to: 'approveWithdraw/'+currentId+'__'+userId,
+        type: 'withdrawal request',
+        message: `Hi Admin,
+        amount: ${currentAmount},
+        user: ${currentUser?.displayName}
+        email: ${currentUser?.email}`,
+      };
+    
+      emailjs.send(
+        'service_ao75urn',
+        'template_tdpbxb7', 
+        templateParams,
+        'MIRKY7yUv_4VJdUdi' 
+      )
+        .then(async() => {
+    
+          await updateDoc(doc(db,'userWithdrawals',currentUser.uid),{
+            withdrawals: arrayUnion({
+              id: currentId,
+              date: Timestamp.now(),
+              status: "pending",
+              amount: amount,
+              user: currentUser.email,
+              plan: plan,
+              address: address,
+              payOption: selectedOption,
+              userId: currentUser.uid,
+              username: currentUser.displayName,
+              isReward: false,
+              isCurrent: true,
+              currentAmount: currentAmount,
+            })
+           });
+          setLoading(false)
+        alert('Withdrawal request has been sent successfully')
+        })
+        .catch((error) => {
+          setLoading(false)
+         alert(error.message)
+          console.error('Error sending email:', error);
+        });
+    } catch (error) {
+  console.log(error)
+  setLoading(false);
+    }
+  }
 
   return (
-    <>
-  {withdrawal?.length>0 ?  <div className='grid grid-cols-1 w-full mt-10 gap-2'>
-     {withdrawal?.length > 0 && slicedData.map((w, i) => (
-            <Withdrawal key={i} {...w}/>
-     )
-     )}
-     <div className='flex justify-center mt-5'>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
-          <button
-            key={pageNumber}
-            className={`px-2 py-1 mr-2 rounded-md text-sm font-medium ${
-              currentPage === pageNumber ? 'bg-[#00eaff3d] text-black font-extrabold' : 'bg-transparent border border-[#00eaff2c]'
-            }`}
-            onClick={() => handlePageChange(pageNumber)}
-          >
-            {pageNumber}
-          </button>
-        ))}
+    <div className='flex w-full'>
+      <Sidebar title='currents'/>
+      <div className='w-full'>
+      <Topbar title={`${currentUser.displayName || ''}/ withdrawal`}/>
+      <div className='p-5 flex flex-col max-sm:items-center gap-5'>
+        <div className='w-full font-bold text-5xl max-sm:text-center'>$<CountUp start={0} end={currentAmount} duration={2} separator=","/></div>
+        <div className='w-full rounded-lg p-5 bg-[#00eaff10] max-sm:text-center mt-5'>Withdraw will be recieved in less than 23hrs</div>
+       <div className='grid grid-cols-2 max-sm:grid-cols-1 w-full gap-5'>
+       <input type="number" onChange={(e)=> setAmount(e.target.value)} placeholder='Enter amount' maxLength={16} className='bg-transparent outline-none placeholder:text-[#a2a1ab] p-3 border border-[#00eaff13] rounded-lg'/>
+        <select className='bg-transparent outline-none text-[#a2a1ab] p-3 border border-[#00eaff13] rounded-lg' name="" id="" value={selectedOption || "Choose withdraw option"} onChange={handleOptionChange}>
+       <option value="Choose withdraw option">Choose withdraw option</option>
+          <option value='BTC'>
+           BTC
+          </option>
+          <option value='BNB'>
+           BNB
+          </option>
+          <option value='USDT'>
+           USDT
+          </option>
+       </select> 
+       <input type="text" onChange={(e)=> setAddress(e.target.value)} placeholder='Enter wallet address' maxLength={32} className='bg-transparent outline-none placeholder:text-[#a2a1ab] p-3 border border-[#00eaff13] rounded-lg'/>
+       </div>
+      {enablebtn && <button onClick={handleWithdraw} className='bg-col text-[#000] font-extrabold sm:w-[300px] w-full p-5 rounded-lg mt-5 hover:opacity-[.7]'>Withdraw</button>}
+      </div>
+      </div>
+      {loading && <div className='w-screen h-screen fixed z-[1111] flex justify-center items-center bg-[#000000ea]'>
+        <img src="/loader.svg" alt="loading.."/></div>}
     </div>
-    </div> : <Empty message={'Withdraw earned money to view your transaction history here'}/>}
-    </>
   )
 }
